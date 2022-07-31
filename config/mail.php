d573d86b3121cc414230c79f00598182d2c5d809
anfu <1435427137@qq.com>
2022-07-31 19:40:45 +0800

    add config

---
+++ config/mail.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    "china" => [
+        "host" => "smtp.111.com",
+        "port" => "25",
+        "username" => "wanganfu@111.com",
+        "password" => "q8cepNy4d7UIzVYE",
+        "nickname" => "anfu"
+    ],
+    "origin" => [
+        "host" => "smtp.111.com",
+        "port" => "25",
+        "username" => "wanganfu@111.com",
+        "password" => "q8cepNy4d7UIzVYE",
+        "nickname" => "anfu"
+    ]
+];
